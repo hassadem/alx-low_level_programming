b4dbb7878ca1e5035b6fc8cec5d65bc80eb2a1f0
hassadem <hassadem001@gmail.com>
2023-03-16 14:46:32 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main - this my main function
+ *
+ * Return: return 0 when successful
+ */
+
+#include <stdio.h>
+int main(void)
+{
+ printf("with proper grammar, but the outcome is a piece of art,");
+ return (0);
+}
