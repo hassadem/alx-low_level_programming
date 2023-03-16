c1eac198e9aab624a05ade4ccedea51f239cc9d6
hassadem <hassadem001@gmail.com>
2023-03-16 16:06:45 -0700

    and that piece

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - this is my main function 
+ *
+ * Return: this return 1 when success
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdur);
+	return (1);
+}
