88b1049833c3ebab10dd96b656dd22f88c0ffa80
hassadem <hassadem001@gmail.com>
2023-03-16 14:14:32 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
-#!/bin/bash
-/*this is my first code really amazing*/
-#include <stdio.h>
+/*main - this is  the main function, return: always 0 */
 
-int main (void) 
+#include <stdio.h>
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
