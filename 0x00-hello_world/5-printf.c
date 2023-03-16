5bd811392be0b7111da991183ef652938ca4fbb3
hassadem <hassadem001@gmail.com>
2023-03-16 15:01:25 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,8 +5,9 @@
  *
  * Return: return 0 when successful
  */
+
 int main(void)
 {
- printf("with proper grammar, but the outcome is a piece of art,\n");
- return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
