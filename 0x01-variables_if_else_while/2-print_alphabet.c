9e36c575180f2fef067526918c4d9fac08355d7a
hassadem <hassadem001@gmail.com>
2023-03-17 11:46:14 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function whhch runs the programm
+ *
+ * Return: always run 0 when success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
