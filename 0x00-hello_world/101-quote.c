4f2e10e07ce676502937f1978d0a8e4b74638bca
hassadem <hassadem001@gmail.com>
2023-03-17 01:12:22 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - this is my main function 
- *
- * Return: this return 1 when success
+ * main-this is my main function 
+ * Return:this return 1 when success
  */
-
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
