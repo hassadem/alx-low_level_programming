3d23cc3d9aa60632f4482a4fc8fd62673d8a64db
hassadem <hassadem001@gmail.com>
2023-03-17 13:15:49 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this main function to run the number program 
+ *
+ * Return: once the code ternimate it print 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 9 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
