98b6e3f95f5c793c523a78717c3b01d45ef5b1ad
hassadem <hassadem001@gmail.com>
2023-03-17 12:44:37 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - this a place where i declare all my variables to run the program
+ *
+ * Return: when all the code run succesfully it returns a value
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	return (0);
+}
