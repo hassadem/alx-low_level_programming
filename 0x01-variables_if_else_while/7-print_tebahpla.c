40b81dbd8a6962db80aebc89e155345d5da9fefa
hassadem <hassadem001@gmail.com>
2023-03-18 13:56:18 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - this a parent function of the program
+ *
+ * Return: it can return whatever
+ */
+
+int main(void) 
+{
+	int i;
+
+	for (i = 'z' ; i = 'a' ; i--)
+		putchar(i);
+	return (0);
+}
