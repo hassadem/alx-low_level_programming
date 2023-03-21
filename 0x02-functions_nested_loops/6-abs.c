0ab9a2f0ee6b665bd4636273c72ab7abb77d5f9d
hassadem <hassadem001@gmail.com>
2023-03-21 13:09:04 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - this is a fucntion to print absolute interger
+ * @int: our parameter
+ * Return: this is to return 0
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (0);
+}
