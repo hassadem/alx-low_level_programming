c5f04bff538ba0cc5954f8a1931708f1a57c15d3
hassadem <hassadem001@gmail.com>
2023-03-22 04:18:03 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - this is our main function
+ *
+ * Return: this is always return 0
+ */
+
+int main(void)
+{
+	int i = 3;
+
+	int first = 1;
+	int second = 2;
+
+	int next = first + second;
+
+	while (i <= 50)
+	{
+		if (i == 50)
+		{
+			printf("%d \n", next);
+		}
+		else
+		{
+			printf("%d, ", next);
+		}
+		first = second;
+		second = next;
+		next = first + second;
+		i++;
+	}
+	return (0);
+}
