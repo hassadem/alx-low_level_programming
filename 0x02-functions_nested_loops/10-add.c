d8fc0b976fd32856889da15dd30189d1c89771fd
hassadem <hassadem001@gmail.com>
2023-03-22 00:47:14 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function to add to integer
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: always 0
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - this is to print out our times table of 9
+ *
+ * Return: always 0
+ */
+
+void times_table(void)
+{
+	
+}
