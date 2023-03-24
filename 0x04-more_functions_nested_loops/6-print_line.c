32fba22978a1908b66c622076ad2b9b4c3533646
hassadem <hassadem001@gmail.com>
2023-03-23 21:47:33 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_line - this is a function to print a straight line
+ * @n: straight line parameter
+ *
+ * Return: 0
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = 1 ; i <= n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
