dd1ee483ef85a4627840d5c66d9bb732a601d630
hassadem <hassadem001@gmail.com>
2023-04-03 15:46:46 -0700

    100-set_string.c

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - to sst pointer to a character value
+ * @s: parameter to point to
+ * @to: parameter to point character to
+ *
+ * Return: always 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
