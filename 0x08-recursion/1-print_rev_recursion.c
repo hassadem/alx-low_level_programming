8702cbaa84c517bc5ee3a17cb0dcc7baf3685be5
hassadem <hassadem001@gmail.com>
2023-04-05 14:26:26 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - fucntion to print string in reverse order
+ * @s: parameter to hold the string
+ *
+ * Return: always 0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
