90f1daafc68afbe39c35c58f3493d354a4260a9e
hassadem <hassadem001@gmail.com>
2023-04-30 00:23:19 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,19 +7,16 @@
  *
  * Return: the number of nodes printed
  */
-size_t print_listint(const listint_t *h);
+size_t print_listint(const listint_t *h)
 {
-	size_t a = 0;
+	size_t n = 0;
 
 	while (h)
 	{
-		if (!h->str)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+		printf("%d\n", h->n);
+		n++;
 		h = h->next;
-		a++;
 	}
 
-	return (a);
+	return (n);
 }
